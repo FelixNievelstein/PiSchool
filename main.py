40fcb4a97585391c0fb8e6b8ce3925ea49bb8c35
Felix Nievelstein <info@felix-nievelstein.de>
2020-01-19 13:28:03 +0100

    add led face test

--- main.py
+++ main.py
@@ -7,6 +7,35 @@ except ImportError:
     exit("This script requires the numpy module\nInstall with: sudo pip install numpy")
 
 from rgbmatrix5x5 import RGBMatrix5x5
+from RPi import GPIO
+from time import sleep
+
+clk = 17
+dt = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+counter = 0
+clkLastState = GPIO.input(clk)
+
+try:
+
+        while True:
+                clkState = GPIO.input(clk)
+                dtState = GPIO.input(dt)
+                if clkState != clkLastState:
+                        if dtState != clkState:
+                                counter += 1
+                        else:
+                                counter -= 1
+                        print counter
+                        setFace(counter)
+                clkLastState = clkState
+                sleep(0.01)
+finally:
+        GPIO.cleanup()
 
 rgbmatrix5x5 = RGBMatrix5x5()
 
@@ -21,8 +50,21 @@ if height == width:
 else:
     delta = 2
 
+def setFace(number):
+    if number is 2
+        r = int(1 * 255.0)
+        g = int(0.0 * 255.0)
+        b = int(0.0 * 255.0)
+        rgbmatrix5x5.set_pixel(0, 0, r, g, b)
+        rgbmatrix5x5.set_pixel(0, 4, r, g, b)
+        rgbmatrix5x5.show()
+    else:
+        for z in list(range(1, 10)[::-1]) + list(range(1, 10)):
+            rgbmatrix5x5.set_pixel(0, 0, r, g, b)
+            rgbmatrix5x5.show()
 
-def make_gaussian(fwhm):
+
+""" def make_gaussian(fwhm):
     x = numpy.arange(0, 5, 1, float)
     y = x[:, numpy.newaxis]
     x0, y0 = 2, 2
@@ -36,21 +78,13 @@ while True:
         fwhm = 5.0 / z
         gauss = make_gaussian(fwhm)
         start = time.time()
-        for y in range(height):
-            for x in range(width):
-                h = 0.5
-                s = 0.8
-                if height <= width:
-                    v = gauss[x, y + delta]
-                else:
-                    v = gauss[x + delta, y]
-                rgb = colorsys.hsv_to_rgb(h, s, v)
-                r = int(rgb[0] * 255.0)
-                g = int(rgb[1] * 255.0)
-                b = int(rgb[2] * 255.0)
-                rgbmatrix5x5.set_pixel(x, y, r, g, b)
+        
+        r = int(1 * 255.0)
+        g = int(0.0 * 255.0)
+        b = int(0.0 * 255.0)
+        rgbmatrix5x5.set_pixel(0, 0, r, g, b)
         rgbmatrix5x5.show()
         end = time.time()
         t = end - start
         if t < 0.04:
-            time.sleep(0.04 - t)
+            time.sleep(0.04 - t) """
